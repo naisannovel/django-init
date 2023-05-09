76c7fa37310ad268c2dcfb51d680970e4fb58102
naisannovel <naisannovel@gmail.com>
2023-05-10 03:03:42 +0600

    update

--- supershop/settings.py
+++ supershop/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'supershop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'supershop',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Naisan@2023'
     }
 }
 
